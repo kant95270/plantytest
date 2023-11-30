dfa20432283bd8a52e9622e479bf8a1d7395ae75
kribi karim <kant95270@gmail.com>
2023-11-30 13:34:19 +0100

    theme enfants

---
+++ wp-content/plugins/essential-blocks/dist/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '6638cc86bf82f40e9a5d');
---
+++ wp-content/plugins/gutenberg/build/block-library/blocks/image/view.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-interactivity'), 'version' => 'af197ff7694844f454d6');
